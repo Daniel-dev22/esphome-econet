1a5e44244b8586b5161acddb3a8ab78df724dac1
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-11 05:28:35 -0400

    Add initial text sensor code

---
+++ components/econet/text_sensor/__init__.py
@@ -0,0 +1,59 @@
+"""
+Text Sensor component for Econet
+"""
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import (
+    CONF_ID,
+    UNIT_CELSIUS,
+    ICON_THERMOMETER,
+    DEVICE_CLASS_SPEED,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_HUMIDITY,
+    STATE_CLASS_MEASUREMENT,
+)
+
+from .. import (
+    econet_ns,
+    CONF_ECONET_ID,
+    ECONET_CLIENT_SCHEMA,
+    EconetClient,
+)
+
+EconetSensor = econet_ns.class_(
+    "EconetSensor", cg.PollingComponent, EconetClient
+)
+
+CONF_ECONET_ID = "econet"
+
+CONF_CC_HVACMODE_TEXT = "cc_hvacmode_text"
+
+CONFIG_SCHEMA = (
+    cv.COMPONENT_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(EconetSensor),
+            cv.Optional(CONF_CC_HVACMODE_TEXT): text_sensor.text_sensor_schema(
+
+            )
+        }
+    )
+    .extend(ECONET_CLIENT_SCHEMA)
+    .extend(cv.polling_component_schema("5s"))
+)
+
+
+
+
+async def to_code(config):
+    """Generate main.cpp code"""
+
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    econet_var = await cg.get_variable(config[CONF_ECONET_ID])
+    cg.add(var.set_econet(econet_var))
+
+    if CONF_CC_HVACMODE_TEXT in config:
+        sens = await text_sensor.new_sensor(config[CONF_CC_HVACMODE_TEXT])
+        cg.add(var.set_cc_hvacmode_text_text_sensor(sens))
