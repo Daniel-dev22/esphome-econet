b2e5027f8d5e7da2a0e9dca4cc7c4c64e6055cbc
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-11 05:56:32 -0400

    Fix ;

--- components/econet/econet.cpp
+++ components/econet/econet.cpp
@@ -206,7 +206,7 @@ void Econet::handle_enumerated_text(uint32_t src_adr, std::string obj_string, ui
 		if(obj_string == "HVACMODE")
 		{
 			cc_hvacmode = value;
-			cc_hvacmode_text = text.c_str()
+			cc_hvacmode_text = text.c_str();
 			ESP_LOGI("econet", "  TextHVACMode : %s ", text.c_str());
 		}
 		else if(obj_string == "AUTOMODE")
