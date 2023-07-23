f6009b00e7c9cfaf59545c5438fd4677a9cf4e99
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-23 17:31:48 -0400

    Syntax

--- components/econet/number/__init__.py
+++ components/econet/number/__init__.py
@@ -45,7 +45,7 @@ CONFIG_SCHEMA = (
             # cv.Optional(CONF_MIN_VALUE): cv.float_, 
             # cv.Optional(CONF_STEP): cv.positive_float, 
              )
-        )}
+        })
         .extend(ECONET_CLIENT_SCHEMA)
         .extend(cv.polling_component_schema("5s")), 
  )
