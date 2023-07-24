6a66c3bce7b8ec566dd9a6c172a6c5b0f2336fa2
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-23 20:05:26 -0400

    Syntax schema

--- components/econet/number/__init__.py
+++ components/econet/number/__init__.py
@@ -39,9 +39,11 @@ CONFIG_SCHEMA = cv.All(
     number.number_schema(EconetNumber) 
     .extend( 
         { 
+            cv.Optional(CONF_CC_DHUMSETP): number.number_schema(
             cv.Required(CONF_MAX_VALUE): cv.float_, 
             cv.Required(CONF_MIN_VALUE): cv.float_, 
             cv.Required(CONF_STEP): cv.positive_float,
+             )
          }
      )
         .extend(ECONET_CLIENT_SCHEMA)
