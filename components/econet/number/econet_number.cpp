edda5a1050cdc40cd413e42fbf7affef0fc79e9b
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-07-27 18:59:48 -0400

    Remove test

--- components/econet/number/econet_number.cpp
+++ components/econet/number/econet_number.cpp
@@ -32,7 +32,6 @@ void EconetNumber::update() {
 }
 
 void EconetNumber::control(float new_dhumsetp) {
-	if(this->econet != nullptr)
 	{
 		this->econet->set_new_dhumsetp(new_dhumsetp);
 		this->publish_state(new_dhumsetp);
