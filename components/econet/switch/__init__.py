7b8b30bcb7e1caab58f3c16a363d54b0e2095493
Daniel <47092714+Daniel-dev22@users.noreply.github.com>
2023-08-26 16:26:54 -0400

    Use loop for switch init

--- components/econet/switch/__init__.py
+++ components/econet/switch/__init__.py
@@ -51,32 +51,17 @@ CONFIG_SCHEMA = (
     .extend(cv.COMPONENT_SCHEMA).extend(cv.polling_component_schema("5s"))
 )
 
-#async def to_code(config):
-   # econet_id = await cg.get_variable(config[CONF_ECONET_ID])
-    #for key in SWITCHES:
-      #  if key in config:
-       #     conf = config[key]
-       #     var = cg.new_Pvariable(conf[CONF_ID])
-            #var = await cg.get_variable(conf[CONF_ID])
-      #      sens = await switch.new_switch(conf)
-      #      await cg.register_component(var, conf)
-      #      cg.add(sens.set_econet(econet_id))
-           # cg.add(getattr(hub, f"set_{key}_number")(var))
-            #cg.add(var.set_switch_id(config[CONF_SWITCH_DATAPOINT])) if key == CONF_ENABLE_SWITCH else ''
-            #cg.add(sens.set_econet(var))
-            #cg.add(getattr(econet_var, f"set_{key}_switch")(var))
 
 async def to_code(config): 
-    var = await cg.get_variable(config[CONF_ECONET_ID])
-    
-    if CONF_ENABLE_SWITCH in config:
-      conf = config[CONF_ENABLE_SWITCH]
-      sens = await switch.new_switch(conf)
-      await cg.register_component(sens, conf)
-      cg.add(sens.set_econet(var))
-      
-    if CONF_CC_DHUM_ENABLE_STATE in config:
-      conf = config[CONF_CC_DHUM_ENABLE_STATE]
-      sens = await switch.new_switch(conf)
-      await cg.register_component(sens, conf)
-      cg.add(sens.set_econet(var))
+
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    econet_var = await cg.get_variable(config[CONF_ECONET_ID])
+    cg.add(var.set_econet(econet_var))
+
+
+    for key in SWITCHES:
+        if key in config:
+            conf = config[key]
+            sens = await sensor.new_switch(conf)
+            cg.add(getattr(var,f"set_{key}_switch")(sens))
